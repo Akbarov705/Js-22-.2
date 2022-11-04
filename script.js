9413dbe3e04fc7a53946f5710fc84b69a4417469
Akbarov705 <111176342+Akbarov705@users.noreply.github.com>
2022-11-04 07:29:18 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+// 1
+let car = [16000, 40000, 3000, 1500, 390000, 12000, 333000, 8000, 714000, 22000, 300, 180000, 13000, 9000]
+
+let cheap = []
+
+console.log(
+
+    car.filter(cheap => cheap <= 35000)
+);
+
+// 2
+let arr = [4, 2, 22, 4, 46, 14, 9, 27, 8, 33, 0, 9]
+
+let even = []
+let odd = []
+
+console.log(
+
+    arr.filter(odd => odd % 2)
+);
+console.log(
+
+    arr.filter(even => even % 3)
+);
+// 3
